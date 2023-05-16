70c5264b1b82f9c97305967037fa3453397082e0
Shua <133736591+apiiee@users.noreply.github.com>
2023-05-16 01:56:25 -0400

    Add files via upload

--- testWork.java
+++ testWork.java
@@ -1,8 +1,11 @@
-public class testWork {
+import java.util.*;
+public class Main {
 
     
     public static void main(String[] args) 
     {
+        Scanner scanInt = new Scanner(System.in);
+        Scanner scanStr = new Scanner(System.in);
         //**************
         // Project 1
         //**************
@@ -10,34 +13,40 @@ public class testWork {
         
         // Variables 
         
-        int rent = 625;
-        int internet = 27;
-        int grocery = 250;
-        int fun = 150;
-        int hourlywage = 10;
+        int rent;
+        int internet;
+        int grocery;
+        int fun;
+        int hourlyWage;
+        String occupation;
         // Math
 
         String Charlie = "poop";
-        System.out.print(Charlie);
+        System.out.println(Charlie);
+
+        System.out.print("Enter how much money you spend a month on rent: ");
+        rent = scanInt.nextInt();
+        System.out.print("Enter how much money you spend a month on internet: ");
+        internet = scanInt.nextInt();
+        System.out.print("Enter how much money you spend a month on groceries: ");
+        grocery = scanInt.nextInt();
+        System.out.print("Enter how much money you spend a month on fun: ");
+        fun = scanInt.nextInt();
+        System.out.print("Enter your hourly wage: ");
+        hourlyWage = scanInt.nextInt();
+        System.out.print("Enter your occupation: ");
+        occupation = scanStr.nextLine();
 
         int total = (rent) + (internet) + (grocery) + (fun);
-        double monthlyHours = total / 10;
+        double monthlyHours = total / hourlyWage;
         double weeklyHours = monthlyHours / 4;
         double dailyHours = weeklyHours / 7;
         double extraMoney = (total + 100) / (10) / (4) / (7);
         // Print out
 
-        System.out.println("Hourly wage is $10");
-        System.out.println("Money for rent is $625");
-        System.out.println("Money for internet is $27");
-        System.out.println("Money for grocery's is $250");
-        System.out.println("Money for fun allowance is $150");
-        System.out.println("The total is $1052");
-        System.out.println("The amount of hours per day a ta/grader would need to work to break even is " + dailyHours);
-        System.out.println("The amount of hours per day a ta/grader would need to work to have an additonal $100 would be " + extraMoney);
+        System.out.println("The amount of hours per day a " + occupation + " would need to work to break even is " + dailyHours);
+        System.out.println("The amount of hours per day a " + occupation + " would need to work to have an additonal $100 would be " + extraMoney);
         System.out.print("***************************************************************************************");
     }
-    
-    
 
 }
